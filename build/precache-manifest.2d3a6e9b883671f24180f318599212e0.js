fce8bd40eec697f7c94be68a605a75b7ea5fec09
Teemu Leivo <teemu.leivo@gofore.com>
2020-05-18 21:57:36 +0300

    Add frontend build

---
+++ build/precache-manifest.2d3a6e9b883671f24180f318599212e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e426ff66937477cb6637b7ec5f46466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30b4034ee72529fcb0b8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "93b7a437827d2b6212f7",
+    "url": "/static/js/2.9c1c8f71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c1c8f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b4034ee72529fcb0b8",
+    "url": "/static/js/main.b00654a1.chunk.js"
+  },
+  {
+    "revision": "d00a55e58aad67ff89c6",
+    "url": "/static/js/runtime-main.01dd18a1.js"
+  }
+]);
